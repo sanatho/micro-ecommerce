d38966f1c2a25403f1b33ad03e5a526625e1547f
Thomas <tsanavia@gmail.com>
2025-10-20 18:09:32 +0200

    added actuator

--- cart-service/src/main/java/com/thomas/cart/config/SecurityConfig.java
+++ cart-service/src/main/java/com/thomas/cart/config/SecurityConfig.java
@@ -16,23 +16,13 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+
                 .authorizeHttpRequests(auth -> auth
+                        .antMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt());
 
         return http.build();
     }
-//
-//    @Bean
-//    public JwtDecoder jwtDecoder() {
-//        // URL HTTP invece di HTTPS
-//        String issuerUri = "http://localhost:9090/realms/ecommerce";
-//        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(issuerUri + "/protocol/openid-connect/certs").build();
-//
-//        // Disabilita validazione issuer (solo dev!)
-//        jwtDecoder.setJwtValidator(JwtValidators.createDefaultWithIssuer(issuerUri));
-//
-//        return jwtDecoder;
-//    }
 }
