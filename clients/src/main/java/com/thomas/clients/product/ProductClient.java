32c1c9ee6c0d0f561c932051efd4f43be116d3a1
thomas <tsanavia@gmailcom>
2025-10-22 17:01:07 +0200

    added setStock

--- clients/src/main/java/com/thomas/clients/product/ProductClient.java
+++ clients/src/main/java/com/thomas/clients/product/ProductClient.java
@@ -1,6 +1,7 @@
 package com.thomas.clients.product;
 
 import com.thomas.clients.product.entity.Product;
+import com.thomas.clients.product.entity.ProductRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -12,28 +13,31 @@ import java.util.List;
 public interface ProductClient {
 
         @GetMapping("/{productId}")
-        public Product getProduct(@PathVariable("productId") Integer productId);
+        Product getProduct(@PathVariable("productId") Integer productId);
 
         @GetMapping("/")
         List<Product> getAllProducts();
 
         @PostMapping
-        public Product saveProduct(@RequestBody Product product);
+        Product saveProduct(@RequestBody Product product);
 
         @DeleteMapping("/{productId}")
-        public void deleteProduct(@PathVariable("productId") Integer productId);
+         void deleteProduct(@PathVariable("productId") Integer productId);
 
         @PatchMapping("/{productId}")
-        public Product editProduct(@RequestBody Product product, @PathVariable("productId") Integer productId);
+        Product editProduct(@RequestBody Product product, @PathVariable("productId") Integer productId);
 
         @GetMapping("/category/{category}")
-        public List<Product> findByCategory(@PathVariable("category") String category);
+        List<Product> findByCategory(@PathVariable("category") String category);
 
         @GetMapping("/brand/{brand}")
-        public List<Product> findByBrand(@PathVariable("brand") String brand);
+        List<Product> findByBrand(@PathVariable("brand") String brand);
 
         @GetMapping("/stock/{product_id}")
-        public Integer getStock(@PathVariable("product_id") Integer product_id);
+        Integer getStock(@PathVariable("product_id") Integer product_id);
+
+        @PostMapping("/stock/")
+        void setStock(@RequestBody ProductRequest product);
 
 }
 
