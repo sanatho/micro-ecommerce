10e48fa1ac530c8046213a78eada696b4b2920bb
thomas <tsanavia@gmailcom>
2025-11-24 22:58:58 +0100

    added securityConfig for the gateway service

---
+++ gateway/src/main/java/com/thomas/gateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.thomas.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/actuator/**", "/public/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
+
